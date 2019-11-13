3a80425441612ba764654024d30cda842e6f5bb6
Hadrien G <knights_of_ni@gmx.com>
2019-11-13 22:50:24 +0100

    Resolve padding bytes UB

--- src/serialization.rs
+++ src/serialization.rs
@@ -29,11 +29,11 @@ use std::{
 
 
 /// A log::Level clone on which I can derive traits
-//
-// NOTE: Mirrors log::Level, must be kept in sync with it
-//
+///
+/// This definition mirrors `log::Level` and must be kept in sync with it.
 #[cfg_attr(test, derive(Arbitrary))]
 #[derive(Abomonation, Clone, Copy, Debug)]
+#[repr(usize)]
 enum Level {
     Error,
     Warn,
@@ -68,27 +68,51 @@ impl Into<log::Level> for Level {
 
 
 /// A log::Record clone (w/o args) on which I can derive traits
-//
-// NOTE: Mirrors log::Record, must be kept in sync with it
-//
+///
+/// `fmt::Arguments` cannot be serialized by abomonation, and must be printed as
+/// a string after the serialized RecordWithoutArgs. We only take note of the
+/// length of that string in RecordWithoutArgs so that we can later deserialize
+/// the string.
+///
+/// This struct mirrors `log::Record`, and should be kept in sync with it as
+/// `log::Record` evolves. For example, it should follow the impeding
+/// stabilization of structured logging in `log` by providing a way to serialize
+/// that information too.
+///
+/// This struct must have no uninitialized padding bytes in order to avoid
+/// triggering UB in `abomonation::encode`. Until that UB issue is resolved,
+/// which will require core Rust changes, please preserve this property while
+/// extending the struct for future `log` version support.
 #[derive(Abomonation)]
-struct RecordWithoutArgs<'a> {
-    level: Level,
-    target: &'a str,
-    // NOTE: args cannot be abomonated and must be printed as a string, we
-    //       only keep track of the length of said string here
-    args_len: usize,
-    module_path: Option<&'a str>,
-    file: Option<&'a str>,
-    line: Option<u32>,
-    // FIXME: Support key_values
+#[repr(C)]  // Used for padding byte avoidance through fine data layout control
+struct RecordWithoutArgs<'a> {      // === PROOF OF ABSENCE OF PADDING BYTES ===
+    level: Level,                   // repr = usize, no padding because on top
+    target: &'a str,                // repr = 2*usize, no padding btw same type
+    args_len: usize,                // repr = usize, no padding btw same type
+    module_path: Option<&'a str>,   // repr = 2*usize due to null ref opt,
+                                    //                no padding btw same type
+    file: Option<&'a str>,          // repr = 2*usize due to null ref opt,
+                                    //                no padding btw same type
+    line: u32,  /* 0 means None */  // repr = u32, <= usize on 32-bit+ CPUs, so
+                                    //             no padding expected.
+                                    //             Also happens to works for
+                                    //             16-bit CPUs, because
+                                    //             8*16-bit above = 4*32-bit, so
+                                    //             we'd stay align on 32-bit
+                                    //             through the fields above.
+    __padding1: u32                 // Needed so that size is multiple of align
+                                    // on 64-bit platforms.
+                                    //
+                                    // + No data recursively serialized by
+                                    //   abomonation (via the &str refs) has
+                                    //   padding bytes, since UTF-8 has align=1.
+    // FIXME: Support key_values, bearing in mind the above constraints
 }
 
 /// Separate the fmt::Arguments from the rest of a log::Record
 ///
 /// fmt::Arguments is too opaque to be serializable, therefore it must be
 /// handled specially by printing it to the output data stream.
-///
 fn split_log_args<'a>(record: &log::Record<'a>) -> (fmt::Arguments<'a>,
                                                     RecordWithoutArgs<'a>) {
     let record_wo_args = RecordWithoutArgs {
@@ -97,7 +121,9 @@ fn split_log_args<'a>(record: &log::Record<'a>) -> (fmt::Arguments<'a>,
         args_len: args_str_len(*record.args()),
         module_path: record.module_path(),
         file: record.file(),
-        line: record.line(),
+        // This is okay because line numbers are 1-based, so 0 can't ever appear
+        line: record.line().unwrap_or(0),
+        __padding1: 0,
         // FIXME: Support key_values
     };
     (*record.args(), record_wo_args)
@@ -107,7 +133,6 @@ fn split_log_args<'a>(record: &log::Record<'a>) -> (fmt::Arguments<'a>,
 ///
 /// We need this both to get advance knowledge of the serialized record size and
 /// to emit the string length before the string in the output data stream.
-///
 fn args_str_len(args: fmt::Arguments<'_>) -> usize {
     use fmt::Write as FmtWrite;
     struct BytesCounter(usize);
@@ -122,19 +147,20 @@ fn args_str_len(args: fmt::Arguments<'_>) -> usize {
     ctr.0
 }
 
-/// Serialize a log::Record.
-///
-/// # Safety
-///
-/// This function uses `abomonation::encode` and inherits its safety issues.
-///
-pub unsafe fn encode_log<W: Write>(record: &log::Record, mut write: W) -> IOResult<()> {
+/// Serialize a log::Record
+pub fn encode_log<W: Write>(record: &log::Record, mut write: W) -> IOResult<()> {
     // Isolate the fmt::Arguments from the input record
     let (record_args, record_wo_args) = split_log_args(record);
 
     // Push everything but the fmt::Arguments. This includes the length of the
     // formatted fmt::Arguments, which we'll use during decoding.
-    abomonation::encode::<RecordWithoutArgs, _>(&record_wo_args, &mut write)?;
+    //
+    // This is safe because we have checked that `RecordWithoutArgs` does not
+    // contain padding bytes, which is the only known source of UB in
+    // `abomonation::encode`.
+    unsafe {
+        abomonation::encode::<RecordWithoutArgs, _>(&record_wo_args, &mut write)?;
+    }
 
     // Print the fmt::Arguments as an UTF-8 string at the end.
     write.write_fmt(record_args)
@@ -150,10 +176,11 @@ pub unsafe fn encode_log<W: Write>(record: &log::Record, mut write: W) -> IOResu
 ///
 /// # Safety
 ///
-/// This function uses `abomonation::decode` and inherits its safety issues.
+/// This function should only be used on bytes that were produced by
+/// `encode_log()`. Passing arbitrary bytes to it can result in undefined
+/// behavior such as null references or invalid UTF-8 in an &str.
 ///
-/// Input bytes must be aligned on a `log_alignment()` boundary.
-///
+/// Furthermore, input bytes must be aligned on a `log_alignment()` boundary.
 pub unsafe fn decode_and_process_log<'a, R>(
     bytes: &'a mut [u8],
     mut process: impl FnMut(&log::Record) -> R
@@ -167,6 +194,7 @@ pub unsafe fn decode_and_process_log<'a, R>(
     let msg = std::str::from_utf8_unchecked(msg_bytes);
 
     // Reconstruct a log::Record and process it
+    let record_line = Some(record_wo_args.line).filter(|&line| line != 0);
     let result = process(
         &log::Record::builder()
                      .args(format_args!("{}", msg))
@@ -174,7 +202,7 @@ pub unsafe fn decode_and_process_log<'a, R>(
                      .target(record_wo_args.target)
                      .module_path(record_wo_args.module_path)
                      .file(record_wo_args.file)
-                     .line(record_wo_args.line)
+                     .line(record_line)
                      // FIXME: Support key_values
                      .build()
     );
@@ -192,6 +220,9 @@ pub fn measure_log(record: &log::Record) -> usize {
 
 /// Report the memory alignment of a serialized log::Record
 pub fn log_alignment() -> usize {
+    // The fact that we print `fmt::Arguments` as an UTF-8 string after the
+    // serialized `RecordWithoutArgs` does not add to the alignment requirements
+    // because an UTF-8 string is byte-aligned, which is the weakest alignment.
     <RecordWithoutArgs as Entomb>::alignment()
 }
 
@@ -201,7 +232,28 @@ mod tests {
     use abomonation::align::AlignedBytes;
     use quickcheck_derive::Arbitrary;
     use quickcheck_macros::quickcheck;
+    use std::mem::size_of;
     use super::RecordWithoutArgs;
+
+    #[test]
+    fn no_record_wo_args_padding() {
+        // Since it is currently UB to send a data structure with uninitialized
+        // padding bytes to abomonation, we rely on the fact that
+        // RecordWithoutArgs does not have padding bytes for safety.
+        //
+        // We manually proved that above, but manual proofs can be wrong, so
+        // let's add a layer of automatic verification.
+        assert_eq!(size_of::<RecordWithoutArgs>(),
+                   size_of::<usize>()          // level: Level is repr(usize)
+                       + 2*size_of::<usize>()  // target: &str is two usizes
+                       + size_of::<usize>()    // args_len: usize
+                       + 2*size_of::<usize>()  // module_path: Option<&str>
+                                               // == &str due to null ref opt
+                       + 2*size_of::<usize>()  // file: Option<&str>, cf above
+                       + size_of::<u32>()      // line: u32
+                       + size_of::<u32>()      // __padding1: u32
+                  );
+    }
 
     /// quickcheck-friendly variant of log::Record
     #[derive(Arbitrary, Clone, Debug)]
@@ -218,7 +270,6 @@ mod tests {
         /// Turn this into a log::Record, process it, and return the result
         ///
         /// We can't return the Record due to fmt::Arguments lifetime issues.
-        ///
         fn process<R>(self, action: impl FnOnce(log::Record) -> R) -> R {
             action(
                 log::Record::builder()
@@ -227,7 +278,8 @@ mod tests {
                     .target(&self.target)
                     .module_path(self.module_path.as_ref().map(String::as_ref))
                     .file(self.file.as_ref().map(String::as_ref))
-                    .line(self.line)
+                    // Zero line numbers don't exist and we exploit this
+                    .line(self.line.filter(|&line| line != 0))
                     // FIXME: Support key_values
                     .build()
             )
@@ -241,12 +293,25 @@ mod tests {
         record.process(|record| {
             // Serialize it into a Vec of bytes
             let mut bytes = Vec::new();
-            unsafe { super::encode_log(&record, &mut bytes) }
-                .expect("Failed to serialize log::Record");
+            super::encode_log(&record, &mut bytes)
+                 .expect("Failed to serialize log::Record");
 
             // Check that the serialization produced as many bytes as expected
             assert_eq!(bytes.len(), super::measure_log(&record),
                        "Serialized record is not as long as expected");
+
+            // Check that the serialized data has no padding bytes. This should
+            // be the case since UTF-8 strings are just initialized bytes with
+            // alignment 1... but hey, better check anyway.
+            //
+            // The fact that RecordWithoutArgs itself does not have padding
+            // bytes is checked in the separate no_record_wo_args_padding test.
+            assert_eq!(bytes.len(),
+                       size_of::<RecordWithoutArgs>()
+                           + record.target().len()
+                           + record.module_path().map(&str::len).unwrap_or(0)
+                           + record.file().map(&str::len).unwrap_or(0)
+                           + super::args_str_len(*record.args()));
 
             // Deserialize it...
             let mut bytes = AlignedBytes::<RecordWithoutArgs>::new(&mut bytes[..]);
